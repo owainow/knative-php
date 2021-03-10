f921f0de9e604b41f991e57213348249332daad7
owainow <owain.osborne@tigerbay.uk.com>
2021-03-10 13:32:15 +0000

    Added populate db sql script.

---
+++ populatedb.sql
@@ -0,0 +1,6 @@
+INSERT INTO TABLE products (Product_ID,Product_Name,Storage_Amount,Service_Provider,Phone_Colour,Camera,Product_Description,Product_Stock)
+  VALUES (1,'iPhone 11', '128GB', 'Vodafone', 'Blue', '1', 'The new iPhone 11 with 128GB Storage space.', 12),
+  VALUES (2,'iPhone X', '128GB', 'Vodafone', 'Silver', '1', 'The new iPhone X with 128GB Storage space.', 15),
+  VALUES (3,'Samsung Galaxy 8', '64GB', 'O2', 'Silver', '1', 'The new Samsung Galaxy 8 in Silver', 100),
+  VALUES (4,'Goolge Pixel 4', '200GB', 'EE', 'Black', '1', 'The new Google Pixel on EE', 28);
+  
