7c0a5b50b60971fc28dfaa17f3d757f783f2583e
owainow <owain.osborne@tigerbay.uk.com>
2021-03-10 12:10:54 +0000

    Updated Sql data types

---
+++ dbcreate.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `products` (
+  `Product_ID` int NOT NULL AUTO_INCREMENT,
+  `Product_Name` varchar(40),
+  `Storage_Amount` int (20),
+  `Service_Provider` varchar (20),
+  `Phone_Colour` varchar(20),
+  `Camera` BOOLEAN NOT NULL DEFAULT '0',
+  `Product_Description` varchar(100),
+  `Product_Stock` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ProductID`),
+);
