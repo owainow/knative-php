20592bceeed9aebdc453f31a2aec5543322ef539
owainow <owain.osborne@tigerbay.uk.com>
2021-03-10 15:14:57 +0000

    Working db scripts for mysql

--- dbcreate.sql
+++ dbcreate.sql
@@ -1,12 +1,12 @@
-CREATE TABLE 'products' (
-  'Product_ID' INT NOT NULL,
-  'Product_Name' varchar(40),
-  'Storage_Amount' varchar (10),
-  'Service_Provider' varchar (20),
-  'Phone_Colour' varchar(20),
-  'Camera' BOOLEAN NOT NULL DEFAULT '0',
-  'Product_Description' varchar(100),
-  'Product_Stock' INT NOT NULL DEFAULT '0',
-  PRIMARY KEY ('Product_ID')
+CREATE TABLE products (
+  Product_ID INT NOT NULL,
+  Product_Name varchar(40),
+  Storage_Amount varchar (10),
+  Service_Provider varchar (20),
+  Phone_Colour varchar(20),
+  Camera BOOLEAN NOT NULL DEFAULT '0',
+  Product_Description varchar(100),
+  Product_Stock INT NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Product_ID`)
 );
 
