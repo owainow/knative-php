6a7fec23719ac656f75d175c830280a37b4d4a15
owainow <owain.osborne@tigerbay.uk.com>
2021-03-10 20:32:21 +0000

    Changed console to index

--- Submit.php
+++ Submit.php
@@ -9,6 +9,6 @@ $_ENV["MYSQL_PASSWORD"],$_ENV["MYSQL_DATABASE"]);
 //Sending form data to sql db.
 mysqli_query($conn, "INSERT INTO products(Product_Name,Stroage_Amount,Service_Provider,Phone_Colour, Camera,Product_Description)
 	VALUES('$_POST[ProuctName]', '$_POST[StorageAmount]', '$_POST[ServiceProvider]', '$_POST[PhoneColour]', '$_POST[Camera]', '$_POST[EnterDescription]')");
-Console.php
+index.php
 
 ?>
